961a505195a8e9bf0a10b8c461cf7efc9a1dbcff
Alejandro Vivas <ajvivas86@gmail.com>
2020-03-08 21:31:08 -0300

    Font family inherit

--- dist/js/toastme.js
+++ dist/js/toastme.js
@@ -1,13 +1,12 @@
 "use strict";
 
 /*!
- * toastmejs v1.2.2
+ * toastmejs v1.2.3
  * Web notifications and dialogs with pure javascript
- * (c) 2019 alexsegen | Twitter https://twitter.com/pixelagil 
+ * (c) 2020 alexsegen | Twitter https://twitter.com/pixelagil 
  * MIT License
  * git+https://github.com/alexsegen/toastmejs.git
  */
-
 (function () {
   function Toastme() {
     var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {
@@ -21,17 +20,20 @@
       theme: "",
       duplicates: false
     };
-
+    debugger;
     this.toastArray = [];
     this.timeout = config.timeout || 5000;
     this.distanceX = config.distanceX || 15;
     this.distanceY = config.distanceY || 15;
     this.positionY = config.positionY || "bottom"; //bottom, top
+
     this.positionX = config.positionX || "right"; //right, left, center
+
     this.zIndex = config.zIndex || 1000;
-    this.ligh = config.ligh || false;
+    this.ligh = config.ligh;
     this.theme = config.theme || "";
-    this.duplicates = config.duplicates || false;
+    this.duplicates = config.duplicates;
+    this.animations = config.animations;
     this.instanceId = "-" + Math.floor(Math.random() * 1000000 + 1);
 
     this.initToast = function () {
@@ -49,9 +51,11 @@
       this.positionX == "center" ? toastmeList.style.right = "calc(50% - 125px)" : toastmeList.style[this.positionX] = this.distanceX + "px";
       toastmeList.style[this.positionY] = this.distanceY + "px";
       toastmeList.style.zIndex = this.zIndex;
+
       if (!document.getElementById("toastmeList" + instanceId)) {
         document.body.appendChild(toastmeList);
       }
+
       return toastmeList;
     };
 
@@ -75,23 +79,15 @@
       var _this = this;
 
       this.initToast();
-
       var toastme = document.createElement("li");
-      toastme.classList.add("toastme", type, this.theme ? this.theme : false, this.ligh ? "ligh" : false);
+      toastme.classList.add("toastme", type, this.theme ? this.theme : false, this.ligh ? "ligh" : false, this.animations === false ? null : "_anim");
       toastme.innerHTML = "\n          <button class=\"toastme-close\"></button>\n          <i class=\"toastme-ico\"></i>\n          <div class=\"toastme-content\">" + str + "</div>";
-
       setTimeout((function () {
         //toastme.classList.add("fade-out")
         toastme.remove();
+
         _this.destroyList(instanceId, str, type);
       }), this.timeout);
-      /*
-      setTimeout(() => {
-        toastme.remove();
-        this.destroyList(instanceId);
-      }, this.timeout + 600);
-      */
-
       return toastme;
     };
 
@@ -102,28 +98,35 @@
         return false;
       }
 
-      this.toastArray.push({ type: type, str: str, instanceId: instanceId });
-
+      this.toastArray.push({
+        type: type,
+        str: str,
+        instanceId: instanceId
+      });
       this.createToastList(this.instanceId);
       document.getElementById("toastmeList" + this.instanceId).appendChild(this.buildToast(type, str, this.instanceId));
     };
+
     this.default = function (str) {
       this.showToast("default", str);
     };
+
     this.success = function (str) {
       this.showToast("success", str);
     };
+
     this.error = function (str) {
       this.showToast("error", str);
     };
+
     this.warning = function (str) {
       this.showToast("warning", str);
     };
+
     this.info = function (str) {
       this.showToast("info", str);
-    };
+    }; //Toastme Dialogs
 
-    //Toastme Dialogs
 
     this.initDialog = function () {
       document.addEventListener("click", (function (e) {
@@ -139,7 +142,6 @@
               item.classList.add("toastOut");
             }));
           }), 400);
-
           setTimeout((function () {
             var array = document.querySelectorAll(".toastme-dialog-bg");
             array.forEach((function (item) {
@@ -150,10 +152,16 @@
       }));
     };
 
-    this.buildDialog = function (title, text, textConfirm, textCancel, showCancel, type, dark) {
+    this.buildDialog = function (title, text) {
+      var textConfirm = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "Confirm";
+      var textCancel = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "Cancel";
+      var showCancel = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : true;
+      var type = arguments[5];
+      var dark = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : false;
+      var animations = arguments.length > 7 && arguments[7] !== undefined ? arguments[7] : true;
       var showTitle = title ? "<p class=\"toastme-dialog-title\">" + title + "</p>" : "";
       var showText = text ? "<p class=\"toastme-dialog-text\">" + text + "</p>" : "";
-      var showType = this.selectType(type) ? this.selectType(type) : "";
+      var showType = this.selectType(type, animations) ? this.selectType(type, animations) : "";
       var btnCancel = showCancel ? "<button id=\"toastmeCancel\" class=\"btn-toastme --toastme-dialog-action --toastme-cancel\">" + (textCancel || "Cancel") + "</button>" : "";
       var dialog = document.createElement("div");
       dialog.setAttribute("id", "toastme-dialog-bg");
@@ -162,18 +170,23 @@
       return dialog;
     };
 
-    this.selectType = function (str) {
+    this.selectType = function (str, animations) {
       switch (str) {
         case "danger":
-          return "<div class=\"toastme-dialog-ico danger\"></div>";
+          return "<div class=\"toastme-dialog-ico danger " + (animations ? '_anim' : '') + "\"></div>";
+
         case "success":
-          return "<div class=\"toastme-dialog-ico success\"></div>";
+          return "<div class=\"toastme-dialog-ico success " + (animations ? '_anim' : '') + "\"></div>";
+
         case "info":
-          return "<div class=\"toastme-dialog-ico info\"></div>";
+          return "<div class=\"toastme-dialog-ico info " + (animations ? '_anim' : '') + "\"></div>";
+
         case "warning":
-          return "<div class=\"toastme-dialog-ico warning\"></div>";
+          return "<div class=\"toastme-dialog-ico warning " + (animations ? '_anim' : '') + "\"></div>";
+
         case "question":
-          return "<div class=\"toastme-dialog-ico question\"></div>";
+          return "<div class=\"toastme-dialog-ico question " + (animations ? '_anim' : '') + "\"></div>";
+
         default:
           return false;
       }
@@ -187,18 +200,18 @@
         textCancel: "" || "Cancel",
         showCancel: true,
         type: null || null,
-        dark: false
+        dark: false,
+        animations: true
       };
-
       this.closeAllDialogs();
       this.initDialog();
-      document.body.appendChild(this.buildDialog(config.title, config.text, config.textConfirm, config.textCancel, config.showCancel, config.type, config.dark));
-
+      document.body.appendChild(this.buildDialog(config.title, config.text, config.textConfirm, config.textCancel, config.showCancel, config.type, config.dark, config.animations));
       return new Promise(function (resolve, reject) {
         document.getElementById("toastmeConfirm").addEventListener("click", (function () {
           resolve(true);
         }));
         var btnCancel = document.getElementById("toastmeCancel");
+
         if (btnCancel) {
           btnCancel.addEventListener("click", (function () {
             resolve(false);
@@ -230,6 +243,7 @@
       Toastme: Toastme,
       toastme: toastme
     };
+    module.exports = toastme;
   } else {
     window.toastme = toastme;
     window.Toastme = Toastme;
